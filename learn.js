ef4a83d9f212313657f7b979c7398eb1f6f58cdd
sillman96 <sillman96@gmail.com>
2017-09-22 08:42:38 -0400

    copy to new repository

---
+++ learn.js
@@ -0,0 +1,15 @@
+var box = {};
+
+box.innerBox = {};
+
+box.innerBox.babyBox = {};
+
+box.innerBox['babyBox'].says = "What's up?";
+
+var bb = box.innerBox.babyBox;
+
+console.log(bb['says'] + " guys?");
+
+
+
+
